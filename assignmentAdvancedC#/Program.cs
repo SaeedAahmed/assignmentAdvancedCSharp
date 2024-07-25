40bd31880abc7544a8aa36684f82a955467ecf0b
SaeedAahmed <ss7377991@gmail.com>
2024-07-25 22:53:47 +0300

    solve all

--- assignmentAdvancedC#/Program.cs
+++ assignmentAdvancedC#/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace assignmentAdvancedC_
+﻿using assignmentAdvancedC_.task_two;
+using assignmentAdvancedC_.TaskOne;
+
+namespace assignmentAdvancedC_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region task one
+            //int[] arr = { 5, 9, 2, 15, 2, 6, 4, 1 };
+
+            //Console.WriteLine("List before sort");
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine("\nList after sort");
+            //Sort.BubbleSort(arr); 
+            #endregion
+
+            #region Task Two
+            //Range<int> range = new Range<int>(10, 1);
+            //Console.WriteLine(range.IsInRange(5));
+            //Console.WriteLine(range.Length());
+
+
+            //Range<double> range02 = new Range<double>(5.2, 2.3);
+            //Console.WriteLine(range02.IsInRange(3.3));
+            //Console.WriteLine(range02.Length());
+            #endregion
         }
     }
 }
---
+++ assignmentAdvancedC#/task two/Range.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignmentAdvancedC_.task_two
+{
+    internal class Range <T> where T : IComparable<T>
+    {
+        public T Max { get; set; }
+        public T Min { get; set; }
+
+        public Range( T max , T min )
+        {
+            Max = max; 
+            Min = min;
+        }
+
+        public bool IsInRange(T value)
+        {
+            return value.CompareTo(Max) <= 0 && value.CompareTo(Min) >= 0;
+        }
+        public T Length()
+        {
+            dynamic max = Max; 
+            dynamic min = Min;
+            return max - min;
+        }
+    }
+}
